a269f0bcbda831f90a6f90fea559f124674bc05e
547350470 <547350470@qq.com>
2016-12-03 21:29:28 +0800

    Add files via upload

---
+++ Vec.cpp
@@ -0,0 +1,26 @@
+#include "Vec.h"
+template <typename T>
+pair<T*, T*> Vec<T>::alloc_n_copy(const T*, const T*)
+{
+	auto data = alloc.allocate(end - begin); 
+	return{ data, uninitializer_copy(begin,end,data) };
+}
+template <typename T>
+Vec<T>::Vec(initializer_list<T> il)
+{
+	auto newdata = alloc_n_copy(il.begin(), il.end());
+	elemnts = newdata.first;
+	first_free = cap = newdata.second;
+}
+template <typename T>
+Vec<T>::Vec(const Vec<T> &v)
+{
+	auto newdata = alloc_n_copy(v.begin(), v.end());
+	elements = newdata.first;
+	first_free = cap = newdata.second;
+}
+template <typename	T>
+Vec<T>::Vec(Vec<T> &&v)throw() :elements(v.elements), first_free(v.first_free), cap(v.cap)
+{
+	v.elements = v.first_free = v.cap = nullptr;
+}
